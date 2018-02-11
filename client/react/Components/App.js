030b3121a8d6ecda919f8bd2b8f63ff339690a16
Owner <owner@MacBook-Pro.local>
2018-02-11 16:56:29 -0500

    Boiler Plate React App with Redux/React Router and SASS along with a Express Server in the back end

---
+++ client/react/Components/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+const css = require('./App.scss');
+
+class App extends Component {
+    render(){
+        return(
+            <div>
+                <h1>React/Redux/React-Router with SASS Front-End</h1>
+                <h1>NodeJS/ExpressJS Back-End</h1>
+                <h2>Happy Hacking!</h2>
+            </div>
+        )
+    }
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const path = require('path');
+
+const DIST = path.join(__dirname, '/client/dist');
+
+module.exports = {
+  entry: path.join(__dirname,'/client/react'),
+  output: {
+    path: DIST,
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: path.join(__dirname,'/client/'),
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+        test:/\.scss$/,
+        use: ExtractTextPlugin.extract({
+            fallback: "style-loader",
+            use: ["css-loader", "sass-loader"]
+          })
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin("Style.css"),
+  ]
+};
